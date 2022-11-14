a5e36b44b02c49c201d100d5bb6964e755e41292
levy52 <radeklewicki52@gmail.com>
2022-11-14 08:30:41 +0100

    add files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.navbar')
+    const allNavLink = document.querySelectorAll('.nav-link')
+    const navbarCollapse = document.querySelector('.navbar-collapse')
+
+	function addShadow() {
+		if (window.scrollY >= 200) {
+			nav.classList.add('shadow-bg')
+		} else {
+			nav.classList.remove('shadow-bg')
+		}
+	}
+
+    allNavLink.forEach(link => link.addEventListener('click', () => navbarCollapse.classList.remove('show')))
+
+
+
+	window.addEventListener('scroll', addShadow)
+})
+
